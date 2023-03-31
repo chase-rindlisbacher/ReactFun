b066f770265c51237ea4ceef71d02d66992b4e49
chase.rindlisbacher <cjrjazz20@gmail.com>
2023-03-31 14:11:30 -0600

    click and increment

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
+function GoButton(props:any) {
+  
+  return (
+    <div>
+      <button onClick={props.numClick /* Pointer to a function */}>Go!</button>
+      <br />
+    </div>
+  )
+}
+
+function AmountOfGo (props: any) {
+  return (
+    <label>You've pressed the button: {props.num} time(s)!</label>
+  )
+}
+
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GoButton numClick={() => setCount(count + 1)} />
+      <AmountOfGo num={count} />
     </div>
   );
 }
